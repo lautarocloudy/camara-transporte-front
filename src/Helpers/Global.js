330db3d8a4d7f2f4c77065ca063b292032c87309
lautarocloudy <119749975+lautarocloudy@users.noreply.github.com>
2025-07-29 20:19:48 -0300

    Update Global.js

--- src/Helpers/Global.js
+++ src/Helpers/Global.js
@@ -1,6 +1,6 @@
 const Global =  {
     //url: "http://localhost:3000/api/"
-     url:"https://api-cuenta-corrientes.vercel.app/api/"
+     url:"https://camara-transporte-back.vercel.app/api/"
    }
    
    export default Global
