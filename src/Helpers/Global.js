c2a49c5f329123fe08f606ad87f7fc3c78e090c6
lautarocloudy <119749975+lautarocloudy@users.noreply.github.com>
2025-07-29 19:28:01 -0300

    Update Global.js

--- src/Helpers/Global.js
+++ src/Helpers/Global.js
@@ -1,6 +1,6 @@
 const Global =  {
-    url: "http://localhost:3000/api/"
-    // url:"https://smartceo-back.vercel.app/api/"
+    //url: "http://localhost:3000/api/"
+     url:"https://api-cuenta-corrientes.vercel.app/api/"
    }
    
    export default Global
